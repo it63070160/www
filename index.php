604ea009b9f4b27bb5bbc338749c77c90d099082
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-10-30 12:18:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     </ul>
     <h2>Some Pictures here !</h2>
     <img src="cat.jpg">
+    <img src="cat2.jpg">
+    <h3><a href="http://www.it.kmitl.ac.th">IT KMITL Link</a>
   </body>
 </html>
   
