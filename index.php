9cc564a3871d3ae96fe5f717cb02e78c43034603
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-10-30 12:12:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT"<br>;
-?>
+<html>
+  <head>
+    <title>ITF-Veerawat</title>
+  </head>
+  <body>
+    <h1>Name</h1>
+    <ul>
+      <li>Firstname: Veerawat</li>
+      <li>Surname: Kamolchet</li>
+    </ul>
+    <h2>Some Pictures here !</h2>
+    <img src="cat.jpg">
+  </body>
+</html>
+  
