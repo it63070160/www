349c5a71d95095036e53a06a68c02d94cf520525
it63070160 <71114745+it63070160@users.noreply.github.com>
2020-10-30 12:22:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     </ul>
     <h2>Some Pictures here !</h2>
     <img src="cat.jpg">
-    <img src="cat2.jpg">
+    <img src="cat2.jpg" width="225" height="224">
     <h3><a href="http://www.it.kmitl.ac.th">เว็บไซต์คณะเทคโนโลยีสารสนเทศ</a></h3>
     <a href="https://onlearn.it.kmitl.ac.th">Onlearn IT KMITL</a>
   </body>
